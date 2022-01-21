ac8f4f6ffe1f20be5a8578c8625d46a2f9a44a71
Lloyd Butler <lloydbutler@live.co.uk>
2022-01-21 08:23:24 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,160 @@
+"""
+This script is used to automate the creation of labs in CML based on user
+requirements.
+"""
+
+
+from getpass import getpass
+import configparser
+import base64
+import requests
+import urllib3
+
+
+def createcredentials():
+    """
+    Generates config.ini on first use that stores user credentials.
+    user must input user name and password as setup in CML, the
+    lab address needs to be that as shown in browser.
+    """
+    username = input("Enter username: ")
+    encoded_password = base64.b64encode(
+        bytes(getpass("Enter password: "), encoding="UTF-8"))
+    encoded_string = str(encoded_password, 'UTF-8')
+
+    lab_address = input("Enter Lab address: ")
+
+    config_create = configparser.ConfigParser()
+
+    config_create["USER"] = {
+        "username": f"{username}",
+        "password": f"{encoded_string}",
+        "lab": f"{lab_address}"
+    }
+
+    with open('config.ini', 'w', encoding='UTF-8') as conf:
+        config_create.write(conf)
+
+
+def authenticate(username, password, address):
+    """
+    Gets bearer token to allow API access. Token is created when corect
+    username and password is stored in config.ini file.
+    """
+
+    decoded_password = base64.b64decode(password)
+    password = decoded_password.decode('UTF-8')
+    url = fr"https://{address}/api/v0/authenticate"
+    payload = {
+        'username': f'{username}',
+        'password': f'{password}'
+    }
+    response_api = requests.request("POST", url, json=payload, verify=False,)
+    return response_api.text
+
+
+def createlab(token, address):
+    """
+    Creates a new lab in CML.
+    user is to enter a name for the lab.
+    """
+    lab_name = input("Lab Name: ")
+    url = fr"https://{address}/api/v0/labs?title={lab_name}"
+    auth = {'Authorization': f'Bearer {token}'}
+    response_api = requests.request("POST", url, headers=auth, verify=False)
+    lab_id = response_api.json()['id']
+    return lab_id
+
+
+def showalllabs(token, address):
+    """
+    Shows user all labs that can edited with their credentials.
+    List will include newly created labs and display the lab id.
+    """
+    url = fr"https://{address}/api/v0/labs?show_all=true"
+    auth = {'Authorization': f'Bearer {token}'}
+    response_api = requests.request("GET", url, headers=auth, verify=False)
+    lab_id = response_api.json()
+    return lab_id
+
+
+def addnodes(token, lab, node, nodequantity, address):
+    """
+    Add nodes to selected lab.
+    node data is pulled from the node database that is created
+    when script first runs.
+    Node labels are suffixed with a number to aid identification.
+    """
+    for i in range(nodequantity):
+
+        url = fr"https://{address}/api/v0/labs/{lab}/nodes?populate_interfaces=true"
+        auth = {'Authorization': f'Bearer {token}'}
+        body = {
+            "x": 0 + (i * 10),
+            "y": 0 + (i * 10),
+            "label": f"{node['data']['label']} - {i}",
+            "configuration": f"{node['data']['configuration']}",
+            "node_definition": f"{node['data']['node_definition']}",
+            "image_definition": f"{node['data']['image_definition']}",
+            "ram": None,
+            "cpus": None,
+            "cpu_limit": None,
+            "data_volume": None,
+            "boot_disk_size": None,
+            "tags": [
+                "string"
+            ]
+        }
+        response_api = requests.request(
+            "POST", url, headers=auth, json=body, verify=False)
+        if response_api.status_code == 200:
+            print(f"{node['data']['label']} - added to {lab}")
+
+
+def labnodes(token, address):
+    """
+    Creates list of available nodes to the user and
+    stores the node configuraions. This is run at the
+    start of program and will only display available nodes.
+    """
+    node_available = []
+    url = fr"https://{address}/api/v0/labs/10ace9/nodes"
+    auth = {'Authorization': f'Bearer {token}'}
+    response_api = requests.request("GET", url, headers=auth, verify=False)
+    nodes = response_api.json()
+
+    for i in range(len(nodes)):
+        url = fr"https://{address}/api/v0/labs/10ace9/nodes/{nodes[i]}?simplified=false"
+        auth = {'Authorization': f'Bearer {token}'}
+        response_api = requests.request("GET", url, headers=auth, verify=False)
+        node_available.append(response_api.json())
+
+    return node_available
+
+
+if __name__ == "__main__":
+    urllib3.disable_warnings()
+    startup = input("Do you need a config file? (Y/N): ").upper()
+    if startup == "Y":
+        createcredentials()
+    read_config = configparser.ConfigParser()
+    read_config.read("config.ini")
+    credentials = read_config["USER"]
+    bearer_token = authenticate(
+        credentials["username"], credentials["password"], credentials["lab"]).strip('"')
+    node_detail = labnodes(bearer_token, credentials["lab"])
+    newlab = input("create new lab? Y/N: ")
+    if newlab == 'Y':
+        createlab(bearer_token, credentials["lab"])
+    lab_ids = showalllabs(bearer_token, credentials["lab"])
+    print("select lab to edit: ")
+    for lab_list in range(len(lab_ids)):
+        print(f"{lab_list} - {lab_ids[lab_list]}")
+    selection = lab_ids[int(input("select: "))]
+    print("select node to add: ")
+    for node_list in range(len(node_detail)):
+        print(
+            f"{node_list} - {node_detail[node_list]['data']['node_definition']}")
+    node_info = node_detail[int(input("select: "))]
+    nodenum = int(input("Number of nodes: "))
+    addnodes(bearer_token, selection, node_info, nodenum, credentials["lab"])
